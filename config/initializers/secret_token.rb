2e5d88a23bc7a649c900973d9f2ec29b936bd0af
selva-oscura <selva_oscura@yahoo.com>
2013-06-18 17:47:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ControllersIntermediateProductAssignment::Application.config.secret_token = '4650ae3d7cfe586d29ea1a1628ea48d5af3ddd1bd3d0f271c6faf6b9e91a4bdc654d64044f17e697d95e6e4ad45e257ca3d33a8a247deecfe7e84678df4fb4b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ControllersIntermediateProductAssignment::Application.config.session_store :cookie_store, key: '_controllers_intermediate_product_assignment_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ControllersIntermediateProductAssignment::Application.config.session_store :active_record_store
